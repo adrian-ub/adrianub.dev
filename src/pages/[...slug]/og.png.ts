400aaaab5a827b0dd1eaf728259e2ab7553bc535
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-10-12 16:45:08 -0500

    feat: update og generator

--- src/pages/[...slug]/og.png.ts
+++ src/pages/[...slug]/og.png.ts
@@ -17,140 +17,96 @@ interface Props {
 }
 
 export async function GET({ props }: Props) {
-  const logo = await fs.readFile(path.resolve('src/assets/logo-dark.png'))
+  const logo = await fs.readFile(path.resolve('src/assets/profile.png'))
 
   const html = {
     type: 'div',
     props: {
       style: {
-        height: '100%',
-        width: '100%',
+        backgroundColor: '#0D0F14',
         display: 'flex',
-        flexDirection: 'row',
-        backgroundColor: 'black',
+        flexDirection: 'column',
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+        fontSize: '2rem',
         color: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden',
       },
       children: [
+        {
+          type: 'img',
+          props: {
+            style: {
+              position: 'absolute',
+              top: '1.5rem',
+              borderRadius: '50%',
+            },
+            width: '80',
+            src: logo.buffer,
+          },
+        },
         {
           type: 'div',
           props: {
             style: {
+              background: 'rgba(0, 0, 0, 0.5)',
+              borderRadius: '24px',
               display: 'flex',
               flexDirection: 'column',
-              justifyContent: 'space-between',
-              padding: '40px',
-              width: '50%',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginTop: '-3.5rem',
+              paddingLeft: '12px',
+              paddingRight: '12px',
+              paddingTop: '26px',
+              paddingBottom: '24px',
+              width: '90%',
             },
             children: [
               {
                 type: 'div',
                 props: {
                   style: {
-                    position: 'relative',
-                    width: '60px',
-                    height: '60px',
-                    display: 'flex',
-                    backgroundColor: 'black',
-                    alignItems: 'center',
-                    padding: '0.8rem',
-                    borderRadius: '.375rem',
-                    filter: 'drop-shadow(white -1px -1px 2px)',
+                    fontSize: '4rem',
+                    color: 'white',
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    whiteSpace: 'pre-wrap',
                   },
-                  children: [
-                    {
-                      type: 'img',
-                      props: {
-                        src: logo.buffer,
-                        style: {
-                          filter: 'none',
-                        },
-                      },
-                    },
-                  ],
+                  children: props.entry.data.title,
                 },
               },
               {
                 type: 'div',
                 props: {
                   style: {
-                    marginTop: '40px',
-                    display: 'flex',
-                    flexDirection: 'column',
+                    marginTop: '6px',
+                    fontSize: '2rem',
+                    color: '#b3b3b3',
+                    whiteSpace: 'pre-wrap',
+                    textAlign: 'center',
                   },
-                  children: [
-                    {
-                      type: 'h1',
-                      props: {
-                        style: {
-                          fontSize: '4rem',
-                          fontWeight: 'bold',
-                          marginBottom: '20px',
-                        },
-                        children: props.entry.data.title,
-                      },
-                    },
-                    {
-                      type: 'p',
-                      props: {
-                        style: {
-                          fontSize: '1.8rem',
-                          color: '#888',
-                          lineHeight: '1.75rem',
-                        },
-                        children: props.entry.data.description || 'Artículos sobre Angular, NestJS, componentes web y más cosas relacionadas con el desarrollo web...',
-                      },
-                    },
-                  ],
+                  children: props.entry.data.description,
                 },
               },
             ],
           },
         },
-        // TODO: enable when configure cover image in frontmatter
-        // {
-        //   type: 'div',
-        //   props: {
-        //     style: {
-        //       width: '50%',
-        //       height: '100%',
-        //       position: 'relative',
-        //       overflow: 'hidden',
-        //       display: 'flex',
-        //     },
-        //     children: [
-        //       {
-        //         type: 'div',
-        //         props: {
-        //           style: {
-        //             position: 'absolute',
-        //             top: 0,
-        //             right: 0,
-        //             bottom: 0,
-        //             left: 0,
-        //             transform: 'skew(-10deg) translateX(20%)',
-        //             display: 'flex',
-        //           },
-        //           children: [
-        //             {
-        //               type: 'img',
-        //               props: {
-        //                 src: '',
-        //                 style: {
-        //                   position: 'absolute',
-        //                   top: '0',
-        //                   left: '30%',
-        //                   width: '100%',
-        //                   height: '100%',
-        //                   objectFit: 'cover',
-        //                 },
-        //               },
-        //             },
-        //           ],
-        //         },
-        //       },
-        //     ],
-        //   },
-        // },
+        {
+          type: 'p',
+          props: {
+            style: {
+              position: 'absolute',
+              bottom: '0',
+              borderBottom: '2px solid white',
+              fontSize: '26px',
+            },
+            children: 'adrianub.dev',
+          },
+        },
       ],
     },
   }
