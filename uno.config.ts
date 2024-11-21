d9e07bed4d0196727e35c80c3e16c9410057dce1
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-11-20 22:05:10 -0500

    feat: add bluesky

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro-vitesse/theme'
 
 export default defineConfig({
-  safelist: ['lt-md:hidden', 'md:hidden', 'i-ri-lightbulb-line', 'i-ri-article-line', 'i-uil-github-alt', 'i-ri-twitter-x-fill', 'i-la-rss-square', 'i-ri-heart-line'],
+  safelist: ['lt-md:hidden', 'md:hidden', 'i-ri-lightbulb-line', 'i-ri-article-line', 'i-uil-github-alt', 'i-ri-twitter-x-fill', 'i-la-rss-square', 'i-ri-heart-line', 'i-ri-bluesky-line'],
 })
